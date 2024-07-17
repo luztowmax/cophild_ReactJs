1af9b554c2d0777747bdcc042337f4f71a96aa28
luztowmax <jordaninvestment414@gmail.com>
2024-07-17 08:43:29 -0400

    second commit

---
+++ my_cophild_app/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Welcome () {
+  return(
+    <div>
+      <h1>Welcome</h1>
+      <p> This is the begining of the React course</p>
+  
+    
+      <ul> 
+        <li> Debuger</li>
+        <li> Mobile app developer</li>
+        <li> Soft ware Engineer</li>
+      </ul>
+    </div>
+
+  )
+}
+
+
+export default Welcome
